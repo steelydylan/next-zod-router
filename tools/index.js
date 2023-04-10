5c391af7196449df0cb3ea2b54b1236074c191a9
steelydylan <ess_president@me.com>
2023-04-10 12:20:29 +0900

    fix

--- tools/index.js
+++ tools/index.js
@@ -3,18 +3,4 @@ const rimraf = require('rimraf');
 
 // Clean up the dist folder
 rimraf.sync('./types/pages/api');
-
-// Locate d.ts files
-require('ts-node').register({
-  compilerOptions: {
-    "target": "es5",
-    "module": "commonjs",
-    "moduleResolution": "node",
-    "allowJs": true,
-    "strict": true,
-    "esModuleInterop": true,
-    "rootDir": "src",
-    "outDir": "./dist"
-  }
-});
-require('./src/index.ts');
+require('./lib/index.js');
