dc9782506aea7c7d141aebe4c742fedee5b75ae2
steelydylan <ess_president@me.com>
2023-04-12 11:45:35 +0900

    fix

--- src/types/pages/api/sample.d.ts
+++
@@ -1,9 +0,0 @@
-import type { PostHandler, GetHandler } from "../../../../tests/api/sample";
-declare module "@/types/pages/api" {
-    interface PostQuery {
-        "/api/sample": PostHandler;
-    }
-    interface GetQuery {
-        "/api/sample": GetHandler;
-    }
-}
