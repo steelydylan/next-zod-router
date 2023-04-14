6789089f21427baa933400f71918a2c0f435bdab
steelydylan <ess_president@me.com>
2023-04-14 18:23:49 +0900

    chore(pkg): fix export fields

--- src/swr.ts
+++ src/swr.ts
@@ -5,13 +5,14 @@ import useSWR from 'swr';
 type GetOptions<T extends keyof GetQuery> = {
   query?: GetQuery[T]["query"];
   requestInit?: Omit<RequestInit, "body">;
+  defaultValue?: GetQuery[T]["res"];
 }
 
 export function useClientSWR<T extends keyof GetQuery>(key: T, options?: GetOptions<T>) {
   const { data, ...rest } = useSWR([key, 'get', options], () => client.get(key, options));
   return {
     ...rest,
-    data: data?.data,
+    data: data?.data ?? options?.defaultValue ?? null,
     error: data?.error,
   }
 }
